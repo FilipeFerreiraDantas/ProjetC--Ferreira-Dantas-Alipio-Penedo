b5b7d52659fd21b3de8d1fe5cd11b2f6c8d391b6
Joao Paulo Alipio Penedo <joao-paulo.ALIPIO-PENEDO@cpnv.ch>
2018-11-27 15:27:11 +0100

    Continuation du code

--- Pyramid/Test/Form1.cs
+++ Pyramid/Test/Form1.cs
@@ -22,18 +22,7 @@ namespace Pyramid
            
         }
 
-        public string GetRandomSorte()
-        {
-            Array valuesSorte = Enum.GetValues(typeof(CarteGenerator.SorteCarte));
-            Array valuesNumCarte = Enum.GetValues(typeof(CarteGenerator.ValeurCarte));
-
-            var rnd = new Random();
-            int indexSorte = rnd.Next(0, valuesSorte.Length);
-            int indexNumCarte = rnd.Next(0, valuesNumCarte.Length);
-
-            var resultSorte = valuesSorte.GetValue(indexSorte);
-            return resultSorte.ToString();
-        }
+        
 
         private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
@@ -41,19 +30,20 @@ namespace Pyramid
         }
         private void MyPaint(object sender, PaintEventArgs e)
         {
-           // string rndmSorteCarte = ;
-           // string rndmNumCarte = ;
+            // string rndmSorteCarte = ;
+            // string rndmNumCarte = ;
 
-            Bitmap bitmap = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.Pique, CarteGenerator.ValeurCarte.Cinq, 0.5));
+            Bitmap bitmap;
             Bitmap bitmap1 = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.Coeur, CarteGenerator.ValeurCarte.Sept, 0.5));
             Bitmap bitmap2 = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.Carreau, CarteGenerator.ValeurCarte.Huit, 0.5));
-            imgCarte1.Image = bitmap;
-            imgCarte2.Image = bitmap1;
-            imgCarte3.Image = bitmap2;
 
+            string valeurSorte = CarteGenerator.GetRandomSorte();
+
+
+            var Sorte = CarteGenerator.GetRandomSorte();
             for(int i = 1; i<=28; i++)
             {
-               // Bitmap bitmap = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.))
+               bitmap = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.valeurSorte, CarteGenerator.GetRandomNumCard(), 0.5));
             }
 
 
