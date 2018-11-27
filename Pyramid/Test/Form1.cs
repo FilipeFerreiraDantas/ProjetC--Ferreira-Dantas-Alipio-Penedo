5e2f4aa8ddf17d8a4cd03d10ad2669ddb8a3c26d
Joao Paulo Alipio Penedo <joao-paulo.ALIPIO-PENEDO@cpnv.ch>
2018-11-27 15:21:42 +0100

    Mise en commun des documents + avancement du code

--- Pyramid/Test/Form1.cs
+++ Pyramid/Test/Form1.cs
@@ -22,12 +22,28 @@ namespace Pyramid
            
         }
 
+        public string GetRandomSorte()
+        {
+            Array valuesSorte = Enum.GetValues(typeof(CarteGenerator.SorteCarte));
+            Array valuesNumCarte = Enum.GetValues(typeof(CarteGenerator.ValeurCarte));
+
+            var rnd = new Random();
+            int indexSorte = rnd.Next(0, valuesSorte.Length);
+            int indexNumCarte = rnd.Next(0, valuesNumCarte.Length);
+
+            var resultSorte = valuesSorte.GetValue(indexSorte);
+            return resultSorte.ToString();
+        }
+
         private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
         private void MyPaint(object sender, PaintEventArgs e)
         {
+           // string rndmSorteCarte = ;
+           // string rndmNumCarte = ;
+
             Bitmap bitmap = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.Pique, CarteGenerator.ValeurCarte.Cinq, 0.5));
             Bitmap bitmap1 = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.Coeur, CarteGenerator.ValeurCarte.Sept, 0.5));
             Bitmap bitmap2 = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.Carreau, CarteGenerator.ValeurCarte.Huit, 0.5));
@@ -35,12 +51,17 @@ namespace Pyramid
             imgCarte2.Image = bitmap1;
             imgCarte3.Image = bitmap2;
 
+            for(int i = 1; i<=28; i++)
+            {
+               // Bitmap bitmap = new Bitmap(CarteGenerator.getCarte(CarteGenerator.SorteCarte.))
+            }
+
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Console.WriteLine(GetRandomSorte());
         }
     }
 }
